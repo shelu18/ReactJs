dd5bb315643c424e85b208cf4042bbab68a3021a
shailendra parihar <shailendraparihar210156@acropolis.in>
2024-03-30 15:50:24 +0530

    folder initialization

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-S
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("div",{className:"App"},
+React.createElement("div",{id:"second"},
+React.createElement("h1",null,"Hello World")
+)
+);
+console.log(heading);
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(heading);
