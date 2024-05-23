7d46622252038ebdf7387ded39bf965e5a799793
shailendra parihar <shailendraparihar210156@acropolis.in>
2024-05-23 17:12:04 +0530

    working till now

--- Tutorial04/app.js
+++ Tutorial04/app.js
@@ -1,29 +1,75 @@
 //creating a food delivery app
-import React from 'react';
+//foer the the demonstration of the simple props :
+import React from "react";
 import ReactDOM from "react-dom";
-import './style.css';
+import "./style.css";
 
-const Header =() =>{
-    return (
-    <div className ="header">
-    <div className ="logo-container">
-    <img  className="logo" src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-by-simplepixelsl-brandcrowd.png" alt="logo"/>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-by-simplepixelsl-brandcrowd.png"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-    <ul>
-    <li>Home</li>
-    <li>Menu</li>
-    <li>Contact</li>
-    </ul> 
-    </div>
-    </div>
-    );
-}
-const Applayout =() =>{
-    return (
-    <Header/>
-    );
+  );
+};
+
+
+//body
+const Cart = (props) => {
+  return <div className="cart">
     
-}
-const root =ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Applayout/>);
+    <img ClassName="food-img"
+    src ="https://media.istockphoto.com/id/179085494/photo/indian-biryani.jpg?s=612x612&w=0&k=20&c=VJAUfiuavFYB7PXwisvUhLqWFJ20-9m087-czUJp9Fs=" alt="food"/>
+    <h3>{props.ResName}</h3>
+    <h4>{props.DishName}</h4>
+    <p>Price: 310 rs </p>
+    <button>Add to Cart</button>
+
+
+  </div>;
+ 
+};
+//props  understanding 
+// defination of props 
+// props is a object that holds the value of the attributes of the component
+// props is immutable
+// props is used to pass the data from parent component to child component
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div ClassName="search"> Search</div>
+      <div className="res-container">
+        
+        <Cart ResName="guru kripa" DishName="Biryani" />,
+        <Cart  ResName="Apna sweet" DishName="rasgulle"/>
+        <Cart  ResName="Bikaner sweet"/>
+       
+
+      </div>
+    </div>
+  );
+};
+
+const Applayout = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout />);
